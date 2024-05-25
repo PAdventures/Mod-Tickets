57284205e8dea08a9bd4ac339305c30ace5e255a
PAdventures <sdulyea@icloud.com>
2024-05-25 09:47:10 +0100

    Feat: Separate production logs and test logs

--- reciple.test.mjs
+++ reciple.test.mjs
@@ -52,7 +52,7 @@ export const config = {
         disableLogPrefix: false,
         logToFile: {
             enabled: true,
-            logsFolder: './logs',
+            logsFolder: './logs/test',
             file: 'latest.log'
         }
     },
